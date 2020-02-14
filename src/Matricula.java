49270d3e4936c52a879221812db14ce033c68bf6
Treinamento <Treinamento@RD723900.raiadrogasil.com>
2020-02-14 14:20:40 -0300

    parte F

---
+++ src/Matricula.java
@@ -0,0 +1,14 @@
+public class Matricula {
+    private Aluno aluno;
+    private Curso curso;
+    Date dataDoDia = new Date();
+
+    public Matricula(Aluno aluno, Curso curso, Date dataDoDia) {
+        this.aluno = aluno;
+        this.curso = curso;
+        this.dataDoDia = dataDoDia;
+    }
+
+
+
+}
