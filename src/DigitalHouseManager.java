f7245008aa25aff2d33adcd38450ee4e9bb3d02e
Treinamento <Treinamento@RD723900.raiadrogasil.com>
2020-02-14 14:29:26 -0300

    parte G

---
+++ src/DigitalHouseManager.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class DigitalHouseManager {
+
+    List<Aluno> lista = new ArrayList<>();
+    List<Professor> lista2 = new ArrayList<>();
+    List<Curso> lista2 = new ArrayList<>();
+    List<Matricula> lista2 = new ArrayList<>();
+
+
+
+}
